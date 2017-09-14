af0b1264bd16e8d55eb08d85bc4ab9dd1b9bcac6
Gregory Dukes <kcils360@gmail.com>
2017-09-14 14:02:00 -0700

    continuing to build out the game

---
+++ Class01Demo/Class01Demo/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+
+namespace Class01Demo
+{
+    class Program
+    {
+
+        //string x = "seventy-two";
+
+        static void Main(string[] args)
+        {
+            string myMainVariable;
+
+            myMainVariable = "This is my variable";
+            Console.WriteLine("Hello World!");
+            Console.WriteLine(myMainVariable);
+
+            SayHello("Hello");
+            SayHello(72);
+            string words = Console.ReadLine();
+            IterationStatements();
+            Console.WriteLine(words);
+            SelectiveStatements();
+            Console.Read();
+        }
+
+        static void SayHello(string greeting)
+        {
+
+            Console.WriteLine($"This is my greeting: {greeting}");
+        }
+
+        static void SayHello(int greeting)
+        {
+
+            Console.WriteLine($"This is my greeting: {greeting}");
+        }
+
+        static void IterationStatements()
+        {
+            for(int x=0; x < 10; x++)
+            {
+                Console.WriteLine(x);
+            }
+
+            int[] numberArray = new int[] { 72, 42, 21, 38 };
+
+            foreach (int number in numberArray)
+                {
+                Console.WriteLine(number);
+                }
+
+             int z = 0;
+             while (z < 10)
+             {
+                Console.WriteLine(z);
+                    z++;
+             }
+
+            int n = 0;
+            do
+            {
+                Console.WriteLine(n);
+                n++;
+            }
+            while (n < 5);
+
+        }
+
+        static void SelectiveStatements()
+        {
+            Console.WriteLine("What is your name?");
+            string userInput = Console.ReadLine();
+
+            if (string.IsNullOrEmpty(userInput))
+            {
+                Console.WriteLine("Please type your name!!!");
+            } else
+            {
+                Console.WriteLine($"Welcom {userInput}");
+            }
+        }
+    }
+}
---
+++ Class02Demo/Class02Demo/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+
+namespace Class02Demo
+{
+    class Program
+    {
+        //double balance = 5280.00;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Dr. Favorite's ATM World Express!");
+            CashCash();
+            
+        }
+
+        static int CashCash()
+        {
+            try
+            {
+
+                Console.WriteLine("What would you like to do. Enter a message: ");
+                Console.WriteLine("1. Check Balance");
+                Console.WriteLine("2. Withdraw");
+                Console.WriteLine("3. Deposit");
+                int input = Convert.ToInt32(Console.ReadLine());
+                return input;
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Please select from available options");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }finally
+            {
+                Console.WriteLine("Okay,");
+            }
+            return 0;
+        }
+
+        public void ViewBal(double balance)
+        {
+            Console.WriteLine($"Your balance is ${balance}");
+        }
+
+        static public void InputHandler()
+
+        {
+        double balance = 5280.00;
+            int selection = CashCash();
+
+            switch (selection)
+            {
+                case 1:
+                    ViewBal(balance);
+                    break;
+                case 2:
+                    Withdraw(balance);
+                    break;
+                case 3:
+                    Deposit(balance);
+                    break;
+                default:
+                    Console.WriteLine("Please choose from available options.");
+                    InputHandler(balance);
+                    break;
+            }
+
+            if (selection  == 1 )
+            {
+                ViewBal(balance);
+            }
+
+        }
+
+        static public void Withdraw(double balance)
+        {
+            Console.WriteLine("How much would you like to withdraw today?");
+            int withdrawAmount = Convert.ToInt32(Console.ReadLine());
+            balance -= withdrawAmount;
+            ViewBal(balance);
+        }
+
+        static public void Deposit(double balance)
+        {
+            Console.WriteLine("How much would you like to deposit today?");
+            int depositAmount = Convert.ToInt32(Console.ReadLine());
+            balance += depositAmount;
+            ViewBal(balance);
+        }
+    }
+}
