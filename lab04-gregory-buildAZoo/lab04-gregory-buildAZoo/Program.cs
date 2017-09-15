d47b480527aec440868654d51219901411aac24f
Gregory Dukes <kcils360@gmail.com>
2017-09-15 13:25:09 -0700

    changed perigrin falcon properties

--- lab04-gregory-buildAZoo/lab04-gregory-buildAZoo/Program.cs
+++ lab04-gregory-buildAZoo/lab04-gregory-buildAZoo/Program.cs
@@ -16,6 +16,18 @@ namespace lab04gregoryBuildAZoo
 
             josieCat.HouseBroken();
             Console.WriteLine($"Josi Cat has {josieCat.Legs()} legs");
+            Console.WriteLine(josieCat.Birth());
+            Console.WriteLine();
+
+            Swallow montyPython = new Swallow();
+            Console.WriteLine("What is the avreage airspeed velocity of a swallow carrying a coconut?");
+            Console.WriteLine(montyPython.LadenAirSpeed());
+            Console.WriteLine($"Swallows are animals, which all have {montyPython.Eyes()} eyes");
+            Console.WriteLine();
+
+            PeregrineFalcon falcor = new PeregrineFalcon();
+            falcor.MaximumAltitude = 350;
+            Console.WriteLine($"A falcon's maximum altitude is {falcor.MaximumAltitude} feet, and can fly at {falcor.UnladenAirSpeed()} mph. ");
 
             Console.Read();
         }
