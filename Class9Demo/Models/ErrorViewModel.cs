1ca74f5890c751954ef7cadd625da262d4c6240d
Gregory Dukes <kcils360@gmail.com>
2017-09-23 14:07:48 -0700

    pushing css changes

---
+++ Class9Demo/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Class9Demo.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
